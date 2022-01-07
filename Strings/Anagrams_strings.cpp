2672897d533ea1c44302d8b5a87c2393d32cd145
utkarsh09900 <97159294+utkarsh09900@users.noreply.github.com>
2022-01-07 17:28:26 +0530

    Update Anagrams_strings.cpp

--- Strings/Anagrams_strings.cpp
+++ Strings/Anagrams_strings.cpp
@@ -1,157 +1,53 @@
-Approach 1:
+Approach 1: Hash Table
 
-TC: O(N logN)
-class Solution
-{
-    public:
-    //Function is to check whether two strings are anagram of each other or not.
-    bool isAnagram(string a, string b)
-    {
-    // Get lengths of both strings
-    int n1 = a.length();
-    int n2 = b.length();
- 
-    // If length of both strings is not same, then they
-    // cannot be anagram
-    if (n1 != n2)
-        return false;
- 
-    // Sort both the strings
-    sort(a.begin(), a.end());
-    sort(b.begin(), b.end());
- 
-    // Compare sorted strings
-    for (int i = 0; i < n1; i++)
-        if (a[i] != b[i])
-            return false;
-    
-    return true;
-  }
+This idea uses a hash table to record the times of appearances of each letter in the two strings s and t. For each letter in s, it increases the counter by 1 while for each letter in t, it decreases the counter by 1. Finally, all the counters will be 0 if they two are anagrams of each other.
+
+The first implementation uses the built-in unordered_map and takes 36 ms.
+	
+	class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if (s.length() != t.length()) return false;
+        int n = s.length();
+        unordered_map<char, int> counts;
+        for (int i = 0; i < n; i++) {
+            counts[s[i]]++;
+            counts[t[i]]--;
+        }
+        for (auto count : counts)
+            if (count.second) return false;
+        return true;
+    }
 };
-
 
 Approach 2:
-(Count characters)
-
-Method 2 (Count characters) 
-1. This method assumes that the set of possible characters in both strings is small. In the following implementation, it is assumed that the characters are stored using 8 bit and there can be 256 possible characters. 
-
-2. Create count arrays of size 256 for both strings. Initialize all values in count arrays as 0.
-3. Iterate through every character of both strings and increment the count of character in the corresponding count arrays.
-4. Compare count arrays. If both count arrays are same, then return true.
-  
-  TC:O(N)
-  SC:O(N)
-    
-    // C++ program to check if two strings
-    // are anagrams of each other
-#include <bits/stdc++.h>
-using namespace std;
-#define NO_OF_CHARS 256
-
-/* function to check whether two strings are anagram of
-each other */
-bool areAnagram(char* str1, char* str2)
-{
-	// Create 2 count arrays and initialize all values as 0
-	int count1[NO_OF_CHARS] = { 0 };
-	int count2[NO_OF_CHARS] = { 0 };
-	int i;
-
-	// For each character in input strings, increment count
-	// in the corresponding count array
-	for (i = 0; str1[i] && str2[i]; i++) {
-		count1[str1[i]]++;
-		count2[str2[i]]++;
-	}
-
-	// If both strings are of different length. Removing
-	// this condition will make the program fail for strings
-	// like "aaca" and "aca"
-	if (str1[i] || str2[i])
-		return false;
-
-	// Compare count arrays
-	for (i = 0; i < NO_OF_CHARS; i++)
-		if (count1[i] != count2[i])
-			return false;
-
-	return true;
-}
-
-/* Driver code*/
-int main()
-{
-	char str1[] = "geeksforgeeks";
-	char str2[] = "forgeeksgeeks";
-
-	// Function Call
-	if (areAnagram(str1, str2))
-		cout << "The two strings are anagram of each other";
-	else
-		cout << "The two strings are not anagram of each "
-				"other";
-
-	return 0;
-}
-
-3. Optimal Solution:
-
-TC:O(N)
-SC:O(String length)
-
-Using Single Array:
-
-// { Driver Code Starts
-#include <bits/stdc++.h>
-using namespace std;
-
-
- // } Driver Code Ends
-
-class Solution
-{
-    public:
-    //Function is to check whether two strings are anagram of each other or not.
-    bool isAnagram(string a, string b){
-    int arr[26] = {0};
-    
-    if(a.length() != b.length())
-        return 0;
-    
-    for(auto x : a)
-        arr[x - 'a']++;
-    
-    for(auto y : b)
-        arr[y - 'a']--;
-        
-    for(int i = 0; i < 26; i++){
-        if(arr[i] > 0)
-            return 0;
+Since the problem statement says that "the string contains only lowercase alphabets", we can simply use an array to simulate the unordered_map and speed up the code. The following implementation takes 12 ms.
+	
+	class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if (s.length() != t.length()) return false;
+        int n = s.length();
+        int counts[26] = {0};
+        for (int i = 0; i < n; i++) { 
+            counts[s[i] - 'a']++;
+            counts[t[i] - 'a']--;
+        }
+        for (int i = 0; i < 26; i++)
+            if (counts[i]) return false;
+        return true;
     }
-    return 1;    
- 
-}
-
 };
 
-// { Driver Code Starts.
+Approach 3: Sorting
 
-int main() {
-    
-    int t;
+For two anagrams, once they are sorted in a fixed order, they will become the same. This code is much shorter (this idea can be done in just 1 line using Python as here). However, it takes much longer time --- 76 ms in C++.
 
-    cin >> t;
-
-    while(t--){
-        string c, d;
-
-        cin >> c >> d;
-        Solution obj;
-        if(obj.isAnagram(c, d)) cout << "YES" << endl;
-        else cout << "NO" << endl;
+class Solution {
+public:
+    bool isAnagram(string s, string t) { 
+        sort(s.begin(), s.end());
+        sort(t.begin(), t.end());
+        return s == t; 
     }
-
-}
-  // } Driver Code Ends
-
+};
