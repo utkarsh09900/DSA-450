b9288fdc71c9acedc7b2216b19a38185c78066bf
utkarsh09900 <97159294+utkarsh09900@users.noreply.github.com>
2022-01-07 12:27:10 +0530

    Create Triplet_Sum_in_Array.cpp

---
+++ Searching and Sorting/Triplet_Sum_in_Array.cpp
@@ -0,0 +1,72 @@
+// Given an array arr of size n and an integer X. Find if there's a triplet in the array which sums up to the given integer X.
+Input:
+n = 6, X = 13
+arr[] = [1 4 45 6 10 8]
+Output:
+1
+Explanation:
+The triplet {1, 4, 8} in 
+the array sums up to 13.
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+class Solution{
+    public:
+    //Function to find if there exists a triplet in the 
+    //array A[] which sums up to X.
+    bool find3Numbers(int A[], int n, int X)
+{
+    int l, r;
+    //Sorting the elements.
+    sort(A, A + n);
+
+    //Traversing the array elements.
+    for (int i = 0; i < n - 2; i++)
+    {
+        //Taking two pointers. One at element after ith index and another
+        //at the last index.
+        l = i + 1;
+        r = n - 1;
+        while (l < r)
+        {
+            //If sum of elements at indexes i, l and r is equal 
+            //to required number, we return true.
+            if (A[i] + A[l] + A[r] == X)
+                return true;
+            //Else if the sum is less than required number, it means we need
+            //to increase the sum so we increase the left pointer l.
+            else if (A[i] + A[l] + A[r] < X)
+                l++;
+            //Else the sum is more than required number and we need to
+            //decrease the sum so we decrease the right pointer r.
+            else
+                r--;
+        }
+    }
+    //returning false if no triplet sum equal to required number is present.
+    return false;
+  }
+};
+
+// { Driver Code Starts.
+
+int main()
+{
+	int T;
+	cin>>T;
+	while(T--)
+	{
+		int n,X;
+		cin>>n>>X;
+		int i,A[n];
+		for(i=0;i<n;i++)
+			cin>>A[i];
+		Solution ob;
+        cout <<  ob.find3Numbers(A, n, X) << endl;
+    }
+}
+  // } Driver Code Ends
