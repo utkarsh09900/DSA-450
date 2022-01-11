8ca5cbe899d27a95ac05079584f9b3696bd7e3e0
utkarsh09900 <97159294+utkarsh09900@users.noreply.github.com>
2022-01-11 23:31:39 +0530

    Update Reverse_array.cpp

--- Arrays/Reverse_array.cpp
+++ Arrays/Reverse_array.cpp
@@ -14,3 +14,18 @@ string reverseWord(string str){
     }
       return str;
 }
+
+
+If it is asked that reverse the array after the given Mth position, then the all code will be same on;ly difference is that we will change the start position as i= M+1;
+
+string reverseWord(string str, int M){
+    int i=M+1; int j=str.length()-1;
+    for(i=0,j=str.length()-1; i<j; i++,j--)
+    {
+        int temp= str[i];
+        str[i]=str[j];
+        str[j]=temp;
+        
+    }
+      return str;
+}
